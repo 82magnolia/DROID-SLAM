fdc7e74497ab011535ac2661df1ef05eeacb26df
82magnolia <82magnolia@snu.ac.kr>
2023-04-10 12:37:12 +0900

    Added video pre-processing

---
+++ video_preprocess.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VIDEO_DIR=$1
+RESULT_DIR=$2
+VID_H=$3
+VID_W=$4
+FPS=$5
+
+NEW_VIDEO_DIR="${VIDEO_DIR/".mp4"/"_resize.mp4"}"
+
+mkdir $RESULT_DIR
+ffmpeg -i ${VIDEO_DIR} -r ${FPS:-"30"} -s ${VID_W:-"640"}x${VID_H:-"360"} -c:a copy ${NEW_VIDEO_DIR} -sws_flags neighbor
+ffmpeg -i ${NEW_VIDEO_DIR} -q:v 2 ${RESULT_DIR}/image-%3d.jpg
+
