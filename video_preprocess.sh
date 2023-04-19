7fb4c65ae98612d4f403ad406264f84ff1d0ce17
82magnolia <82magnolia@snu.ac.kr>
2023-04-19 19:11:12 +0900

    Fixed fps for better performance

--- video_preprocess.sh
+++ video_preprocess.sh
@@ -9,6 +9,6 @@ FPS=$5
 NEW_VIDEO_DIR="${VIDEO_DIR/".mp4"/"_resize.mp4"}"
 
 mkdir $RESULT_DIR
-ffmpeg -i ${VIDEO_DIR} -r ${FPS:-"30"} -s ${VID_W:-"640"}x${VID_H:-"360"} -c:a copy ${NEW_VIDEO_DIR} -sws_flags neighbor
+ffmpeg -i ${VIDEO_DIR} -r ${FPS:-"60"} -s ${VID_W:-"640"}x${VID_H:-"360"} -c:a copy ${NEW_VIDEO_DIR} -sws_flags neighbor
 ffmpeg -i ${NEW_VIDEO_DIR} -q:v 2 ${RESULT_DIR}/image-%3d.jpg
 
